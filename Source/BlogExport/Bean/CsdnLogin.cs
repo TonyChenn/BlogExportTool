14f0b59873fee91e1eb44c45c118d82416319b3f
tonychenn <852454151@qq.com>
2019-10-15 10:03:10 +0800

    公开源码

---
+++ Source/BlogExport/Bean/CsdnLogin.cs
@@ -0,0 +1,8 @@
+﻿public class CsdnLogin
+{
+    public string loginType { get; set; }
+    public string pwdOrVerifyCode { get; set; }
+    public string userIdentification { get; set; }
+    public string uaToken { get; set; }
+    public string webUmidToken { get; set; }
+}
